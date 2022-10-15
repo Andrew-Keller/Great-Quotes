b068e3f3662997587b6b8a2bf1091d1aed22cb74
Andrew-Keller <112123995+Andrew-Keller@users.noreply.github.com>
2022-10-15 13:37:26 -0400

    Add files via upload

---
+++ csv_util.php
@@ -0,0 +1,30 @@
+function csvFileToArray($fname){
+
+	
+}
+
+
+function csvFileToArrrayPlusReturn($index,$fname){
+
+
+}
+
+function AddRecordToCSV($quote,$authorIndex){
+
+
+}
+
+function ModifyLine($index,$quote,$author){
+
+
+}
+
+function EmptyLine($lineIndex){
+
+
+} 
+
+function DeleteLine($LineIndex){
+
+
+}
