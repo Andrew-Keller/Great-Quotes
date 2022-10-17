e82211417ca4227691748680913e1ff8c973fc4b
Andrew-Keller <112123995+Andrew-Keller@users.noreply.github.com>
2022-10-16 21:04:31 -0400

    Add files via upload

--- csv_util.php
+++ csv_util.php
@@ -1,30 +1,55 @@
+<?php
 function csvFileToArray($fname){
+	if (file_exists($fname)){
+		global $filearray;
+		$filearray= [];
+		$file = fopen($fname, 'r');
+		$i = 0;
+		while ($record = fgetcsv($file)) {
+			$filearray[$i] = array(0 => $record[0],1 => $record[1]);
+			$i=$i+1;}fclose($file);return $filearray;}
 
+	else{return array("File doesn't exist");}}
+
+
+function csvFileToArrayPlusReturn($index,$fname){
+	if (file_exists($fname)){
+		global $filearray;
+		$filearray= [];
+		$file = fopen($fname, 'r');
+		$i = 0;
+		while ($record = fgetcsv($file)) {
+			$filearray[$i] = array(0 => $record[0],1 => $record[1]);
+			$i=$i+1;}fclose($file);return $filearray[$index];}
+
+	else{return array("File doesn't exist");}}
+
+
+
+
+function AddRecordToCSV($fname,$index0,$index1){
+	if (file_exists($fname)){
 	
-}
+		$file = fopen($fname, 'a');
+		fputcsv($file, $index0,$index1);
+		fclose($file);
+		}
 
+	else{return array("File doesn't exist");}}
 
-function csvFileToArrrayPlusReturn($index,$fname){
+function ModifyLine($csv,$index,$quote,$author){
 
-
-}
-
-function AddRecordToCSV($quote,$authorIndex){
-
-
-}
-
-function ModifyLine($index,$quote,$author){
 
 
 }
 
-function EmptyLine($lineIndex){
+function EmptyLine($csv,$lineIndex){
 
 
 } 
 
-function DeleteLine($LineIndex){
+function DeleteLine($csv,$LineIndex){
 
 
 }
+?>
